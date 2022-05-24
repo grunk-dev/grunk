537415a3679c212d4c55d7cb919d95bca2fbf4d3
Jan Kleinert <jan.kleinert@dlr.de>
2022-05-24 17:36:48 +0200

    dont force new ABI

--- conanfile.py
+++ conanfile.py
@@ -21,7 +21,6 @@ class GrunkConan(ConanFile):
             del self.options.fPIC
         self.options["boost"].shared = False
         self.options["boost"].header_only = True
-        self.settings.compiler.libcxx = "libstdc++11"
 
     def build(self):
         cmake = CMake(self)
