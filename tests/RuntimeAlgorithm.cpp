4b25606ce9826b1a4afb83f5b7610791a57f8b35
Jan Kleinert <jan.kleinert@dlr.de>
2022-09-13 09:28:14 +0200

    Reflect::Function -> Reflect::DynamicFunction

--- tests/RuntimeAlgorithm.cpp
+++ tests/RuntimeAlgorithm.cpp
@@ -64,7 +64,7 @@ public:
 
 TEST_F(RuntimeAlgorithmTest, Basic)
 {
-    auto f = Reflect::FunctionImpl(&add, "add");
+    auto f = Reflect::Function(&add, "add");
 
     // l and r are the root input nodes
     auto l = Feature("MyDouble", 0.2);
@@ -112,7 +112,7 @@ TEST_F(RuntimeAlgorithmTest, Basic)
 
 TEST_F(RuntimeAlgorithmTest, PassNonRumtimeFeatureToRuntimeAlgorithm)
 {
-    auto f = Reflect::FunctionImpl(&add, "add");
+    auto f = Reflect::Function(&add, "add");
 
     auto lr = Feature("MyDouble", 0.2);
     auto lc = Feature(MyDouble(0.2));
@@ -129,7 +129,7 @@ TEST_F(RuntimeAlgorithmTest, PassNonRumtimeFeatureToRuntimeAlgorithm)
 
 TEST_F(RuntimeAlgorithmTest, MultiOutput)
 {
-    auto f = Reflect::FunctionImpl(&get_components, "get_components");
+    auto f = Reflect::Function(&get_components, "get_components");
 
     auto i = Feature("Point", 0.2, 0.6);
     auto fun = eval(f, i);
