7c43ecb49aa1addc51e608ceee80be411e821f26
Jan Kleinert <jan.kleinert@dlr.de>
2022-10-26 21:31:53 +0200

    fix creator function

--- grunk/plugins/IPlugin.h
+++ grunk/plugins/IPlugin.h
@@ -14,6 +14,7 @@
 
 #include <reflect/Reflect.hpp>
 #include <boost/dll/alias.hpp> 
+#include <functional>
 
 
 namespace grunk {
@@ -82,20 +83,20 @@ struct IPlugin
      * @brief Destroy the IPlugin object
      */
     virtual ~IPlugin(){}
-};
 
-namespace details {
-
-    // factory function for concrete derived plugins.
-    // This is used in the GRUNK_REGISTER_PLUGIN macro
+    /**
+     * @brief factory function for derived classes
+     * 
+     * @tparam Plugin The plugin to be created. Must be default-constructible and derived from IPlugin
+     * @return std::unique_ptr<IPlugin> the created plugin
+     */
     template <typename Plugin>
-    inline std::unique_ptr<Plugin> create() { \
-        return std::make_unique<Plugin>(); \
+    inline static std::unique_ptr<IPlugin> create() {
+        static_assert(std::is_default_constructible_v<Plugin>, "Can only create default-constructible plugins.");
+        static_assert(std::is_base_of_v<IPlugin, Plugin>, "Can only create plugins derived from IPlugin");
+        return std::make_unique<Plugin>();
     };
-
-    using PluginFactoryFunc = std::unique_ptr<IPlugin>();
-
-} //namespace deteails 
+};
 
 } //namespace grunk
 
@@ -106,6 +107,4 @@ namespace details {
  * @ingroup plugin
  * 
  */
-#define GRUNK_REGISTER_PLUGIN(name) \
-    static_assert(std::is_default_constructible_v<name>); \
-    BOOST_DLL_ALIAS(grunk::details::create<name>, create_grunk_plugin)
+#define GRUNK_REGISTER_PLUGIN(name) BOOST_DLL_ALIAS(grunk::IPlugin::create<name>, create_grunk_plugin)
