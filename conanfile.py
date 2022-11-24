89fc1c9b0c744009b91b4b8ef5e315570dcb69f2
Jan Kleinert <jan.kleinert@dlr.de>
2022-11-24 22:43:24 +0100

    bump reflect version

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,7 @@ class GrunkConan(ConanFile):
     options = {"shared": [True], "fPIC": [True, False]}
     default_options = {"shared": True, "fPIC": True}
     generators = "cmake_find_package"
-    requires = "yaml-cpp/0.7.0", "boost/1.78.0", "parametric/0.2.0", "reflect/0.1.0"
+    requires = "yaml-cpp/0.7.0", "boost/1.78.0", "parametric/0.2.0", "reflect/0.1.1"
     exports_sources = "*"
 
     def config_options(self):
