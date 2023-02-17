7c1ced9f61932a277206c27f99c6e1ac5de09ca1
Jan Kleinert <jan.kleinert@dlr.de>
2023-02-17 15:18:07 +0100

    fix #59

---
+++ grunk/helper/String.cpp
@@ -0,0 +1,21 @@
+#include "String.hpp"
+
+namespace grunk {
+
+namespace helper {
+
+
+String::String(std::string_view s) : str{s} {}
+
+String::operator const char*() const {
+    return str.c_str();
+}
+
+String::operator std::string() const {
+    return str;
+}
+
+
+} // namespace helper
+
+} // namespace grunk
