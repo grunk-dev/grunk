4fd1297d269c353de62c28b9ad2da7abd09d9775
Jan Kleinert <jan.kleinert@dlr.de>
2023-03-16 14:35:44 +0100

    start unit test for dynamic mode

--- tests/meta.cpp
+++ tests/meta.cpp
@@ -34,16 +34,19 @@ public:
 
     static void SetUpTestCase() {
 
-        // reflect::register_type<DynamicFeature>("Feature")
-        // .add_constructor<int>();
+        reflect::register_type<double>("double")
+        .add_constructor<double>();
 
-        // reflect::register_function(
-        //     [](double const& l, double const& r){ return l+r;}, 
-        //     "plus"
-        // );
+        reflect::register_type<DynamicFeature>("Feature")
+        .add_member_function(&DynamicFeature::value, "value");
+
+        reflect::register_function(&f, "f");
+        reflect::register_function(&h, "h");
+        reflect::register_function(&h_meta, "h_meta");
     } 
 
     static void TearDownTestCase() {
+        reflect::get_type_registry().clear();
     } 
 
 };
@@ -69,5 +72,28 @@ TEST_F(MetaTest, static_mode)
         auto fy = Feature("fy", std::move(y)); //to do: make parametric accept const references
         auto z = action("z", &h_meta, fy)->output();
         ASSERT_NEAR(z.value(), 0.125, 1e-15);
+    }
+}
+
+TEST_F(MetaTest, dynamic_mode)
+{
+    {
+        // calling x->f->h in the normal way. 
+        // f should be called when entering the function body of h
+        f_called = false;
+        auto x = Feature("x", "double", 0.5);
+        auto y = action("y", "f", x)->output();
+        auto z = action("z", "h", y)->output();
+        ASSERT_NEAR(z.value().as<double>(), 0.125, 1e-15);
+    }
+    {
+        // calling x->f->wrap-in-feature->h_meta. h_meta accepts a feature node and can
+        // postpone the evaluation of the feature tree.
+        f_called = false;
+        auto x = Feature("x", "double", 0.5);
+        auto y = action("y", "f", x)->output();
+        auto fy = DynamicFeature("fy", reflect::DynamicObject(y)); //to do: this  is not a dynamicFeature
+        auto z = action("z", "h_meta", fy)->output();
+        ASSERT_NEAR(z.value().as<double>(), 0.125, 1e-15);
     }
 }
