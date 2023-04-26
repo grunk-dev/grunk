d2c933ce4a0de65f8b3f96f5c01e6b4bcf1d43d1
Jan Kleinert <jan.kleinert@dlr.de>
2023-04-26 17:21:38 +0200

    update conan command

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ class CMakeBuild(build_ext):
             build_temp.mkdir(parents=True)
 
         subprocess.run(
-            ["conan", "install", ext.sourcedir], cwd=build_temp, check=True
+            ["conan", "install", ext.sourcedir, '--build=missing'], cwd=build_temp, check=True
         )
 
         subprocess.run(
