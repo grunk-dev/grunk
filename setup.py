4be3cb9bafa44adf3e1cb96b07e4585003b7ca9b
Jan Kleinert <jan.kleinert@dlr.de>
2023-04-26 17:45:40 +0200

    [ciskip] install wheel, update pip

--- setup.py
+++ setup.py
@@ -144,6 +144,6 @@ setup(
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
     python_requires=">=3.7",
-    install_requires="conan<2.0.0"
+    install_requires=["conan<2.0.0", "wheel"]
 )
 print(find_packages(where="./python"))
