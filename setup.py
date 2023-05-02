defe724f75dfdeba0a3ba16f86fd1092eac0aaed
Jan Kleinert <jan.kleinert@dlr.de>
2023-05-02 13:25:38 +0200

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -151,6 +151,5 @@ setup(
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
     python_requires=">=3.7",
-    install_requires=["conan<2.0.0", "wheel"]
+    install_requires=["conan<2.0.0"]
 )
-print(find_packages(where="./python"))
