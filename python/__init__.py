1ef492c2bc8065a01c7edb0b2b31197a757c91e2
Jan Kleinert <Jan.Kleinert@dlr.de>
2023-05-16 01:19:41 +0200

    add latest reflect to dll path on windows

--- python/__init__.py
+++ python/__init__.py
@@ -4,8 +4,9 @@ if platform == 'win32':
     from .plugin_manager import get_dll_paths
     from os import add_dll_directory
 
-    #to do: configure version in setup step to fit the one given in conanfile.py
-    for d in get_dll_paths("reflect", "0.1.4", in_grunk_dir=False):
+    #to do: configure reflect version in setup step to fit the one given in conanfile.py
+    #Currently we are taking the latest one (which should be fine under normal circumstances)
+    for d in get_dll_paths("reflect", in_grunk_dir=False):
         add_dll_directory(d)
 
 
