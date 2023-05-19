aa6b709de768941fe6be9f97156bbb3267560f92
Jan Kleinert <jan.kleinert@dlr.de>
2023-05-19 07:30:07 +0200

    copy reflect library to site_packages/grunk

--- python/__init__.py
+++ python/__init__.py
@@ -1,15 +1,3 @@
-from sys import platform
-
-if platform == 'win32':
-    from .plugin_manager import get_dll_paths
-    from os import add_dll_directory
-
-    #to do: configure reflect version in setup step to fit the one given in conanfile.py
-    #Currently we are taking the latest one (which should be fine under normal circumstances)
-    for d in get_dll_paths("reflect", in_grunk_dir=False):
-        add_dll_directory(d)
-
-
 from ._core import __version__ as _version
 __version__ = _version
 from ._core import *
