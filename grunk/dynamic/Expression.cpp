1cf8da140fa80af57877a804c6cdd69d5f5a8418
Jan Kleinert <jan.kleinert@dlr.de>
2023-06-06 15:02:06 +0200

    deserialize Expression

--- grunk/dynamic/Expression.cpp
+++ grunk/dynamic/Expression.cpp
@@ -1,4 +1,5 @@
 #include "Expression.hpp"
+#include <grunk/io/io_error.hpp>
 
 namespace grunk {
 
@@ -15,7 +16,7 @@ Expression::Expression(
     for (auto& i: inputs){
         depends_on(i.param());
     }
-    computes(output, parametric::param<reflect::DynamicObject>(id));
+    computes(out, parametric::param<reflect::DynamicObject>(id));
 }
 
 void Expression::eval() const 
@@ -28,15 +29,15 @@ void Expression::eval() const
     }
 
     double result = parser.Eval().GetFloat();
-    if (!output.expired()) {
-        output.set_value(reflect::DynamicObject(result));
+    if (!out.expired()) {
+        out.set_value(reflect::DynamicObject(result));
     }
 }
 
 std::string Expression::serialize() const
 {
     YAML::Node s;
-    s.push_back(output.param().id());
+    s.push_back(out.param().id());
     s.push_back(expr);
 
     s.SetStyle(YAML::EmitterStyle::Flow);
@@ -45,6 +46,39 @@ std::string Expression::serialize() const
     auto tag = YAML::VerbatimTag("expr");
     out << tag << s;
     return out.c_str();
+}
+
+ExpressionPtr Expression::deserialize(
+    YAML::Node const& node,
+    FeatureContainer const& features
+)
+{
+    std::string expr = node[1].as<std::string>();
+    mup::ParserX p;
+    p.EnableAutoCreateVar(true);
+    p.SetExpr(expr);
+
+    std::vector<DynamicFeature> input_vec;
+    for (auto const& [input_name, var] : p.GetVar()) {
+        auto feature_it = features.find(input_name);
+        if (feature_it == std::end(features)) {
+            throw io_error(
+                "Could not find input "s
+                    + input_name + " in expression " + node[0].as<std::string>()
+                    + " = " + expr
+                    + ". Are the steps in the correct topological order?"
+            );
+        }
+        input_vec.push_back(feature_it->second);
+        // cout << item->first << "=" << (Variable&)(*(item->second)) << "\n";
+    }
+
+    return grunk::expression("", expr, std::move(input_vec));
+}
+
+ExpressionPtr expression(std::string const& id, std::string const& expr, std::vector<DynamicFeature> const& args)
+{
+    return parametric::compute_node_ptr<Expression>(new Expression(id, expr, args));
 }
 
 } // namespace grunk
--- grunk/dynamic/Expression.hpp
+++ grunk/dynamic/Expression.hpp
@@ -1,29 +1,72 @@
 #pragma once 
 
-#include <grunk/core/Action.hpp>
 #include <grunk/dynamic/DynamicFeature.hpp>
+#include <grunk/dynamic/FeatureContainer.hpp>
 
+#include <grunk/parametric_core.hpp>
+#include <yaml-cpp/yaml.h>
 #include "muparserx/mpParser.h"
 
 namespace grunk {
 
+    class Expression;
+
+    using ExpressionPtr = parametric::compute_node_ptr<Expression>;
+
     class Expression : public parametric::ComputeNode
     {
-    public:
         Expression(
             std::string const& id,
             std::string const& expression,
             std::vector<DynamicFeature> const& in
         );
 
+    public:
+
+        // factory method
+        friend ExpressionPtr expression(
+            std::string const& id, 
+            std::string const& expr, 
+            std::vector<DynamicFeature> const& args
+        );
+
         void eval() const override final;
+
+        inline DynamicFeature output(size_t idx = 0) const
+        {
+            return DynamicFeature(out, reflect::resolve<double>());
+        }
 
         std::string serialize() const override final;
 
+        static ExpressionPtr deserialize(
+            YAML::Node const&,
+            FeatureContainer const&
+        );
+
     private:
         std::vector<DynamicFeature> const inputs;
-        parametric::OutputParam<reflect::DynamicObject> mutable output;
+        parametric::OutputParam<reflect::DynamicObject> mutable out;
         std::string const expr;
     };
+
+    ExpressionPtr expression(std::string const& id, std::string const& expr, std::vector<DynamicFeature> const& args);
+
+    template <
+    typename... Args,
+    typename = std::enable_if_t<!(sizeof...(Args) == 1 && (std::is_same_v<std::vector<DynamicFeature>, std::decay_t<Args>> && ...))>
+    >
+    ExpressionPtr expression(std::string const& id, std::string const& expr, Args&&... args)
+    {
+        auto to_feature = [](auto&& arg){
+            using Arg = std::decay_t<decltype(arg)>;
+            if constexpr (details::is_feature_v<Arg>){
+                return arg;
+            } else {
+                return Feature("", reflect::DynamicObject(std::forward<Arg>(arg))); //TODO: Until we properly support unnamed features, this will be an empty string
+            }
+        };
+        return expression(id, expr, std::vector<DynamicFeature>{to_feature(std::forward<Args>(args))...});
+    }
 
 }  // namespace grunk
