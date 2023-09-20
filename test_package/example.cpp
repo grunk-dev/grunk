54496a43cd7362b70bf0115ebe3bce476af1e7ce
Jan Kleinert <jan.kleinert@dlr.de>
2023-09-20 10:38:13 +0200

    adapt test_package to paradigms update

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -15,8 +15,8 @@ int main(int argc, char* argv[]) {
         grunk::Feature y("y", 15.2);
         grunk::Feature z("z", 25.6);
 
-        auto a = grunk::action("a", &add, x, y)->output();
-        auto b = grunk::action("b", &add, a, z)->output();
+        auto a = grunk::action("a", &add, x, y).output();
+        auto b = grunk::action("b", &add, a, z).output();
 
         std::cout << "Until here, nothing has happened" << std::endl;
 
@@ -53,8 +53,8 @@ int main(int argc, char* argv[]) {
         grunk::Feature z("z", "PluginA::MyDouble", 2.0);
 
         // use plugin functions
-        auto a = grunk::action("a", "PluginA::add", x, y)->output();
-        auto b = grunk::action("b", "PluginB::multiply", a, z)->output();
+        auto a = grunk::action("a", "PluginA::add", x, y).output();
+        auto b = grunk::action("b", "PluginB::multiply", a, z).output();
 
         auto b_result = reflect::cast<double>(b.value().get("value"));
         std::cout << b_result << std::endl;
