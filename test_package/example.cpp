3526b5b9090f4976d5cdb7d2ad2e88c9ef4d19d9
Jan Kleinert <jan.kleinert@dlr.de>
2023-11-28 21:13:36 +0100

    typo

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -40,8 +40,8 @@ int main(int argc, char* argv[]) {
     // load plugins
     auto& plugins = grunk::get_plugin_registry();
     plugins.prepend_path(argv[1]);
-    plugins.load("PluginA");
-    plugins.load("PluginB");
+    plugins.load("pluginA");
+    plugins.load("pluginB");
 
     {
         std::cout << "\n\nUnique plugins " << plugins.count() << ":\n";
