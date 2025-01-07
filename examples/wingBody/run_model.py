ae5ab35802c1146cb34d8ddc99fe47fe2709dd49
Jan Kleinert <jan.kleinert@dlr.de>
2025-01-08 00:44:57 +0100

    finish wing modeling

--- examples/wingBody/run_model.py
+++ examples/wingBody/run_model.py
@@ -2,12 +2,12 @@ import grunk
 
 if __name__ == '__main__':
     grunk.init()
-    grunk.get_plugin_registry().load_env("dlrk")
+    grunk.get_plugin_registry().load_env("wingbody")
     
     
     recipe = grunk.read("wing_body.grr.yml")
 
-    for key in ["nose", "central_fuselage", "tail_mantle", "tail_back", "fuselage_face"]:
+    for key in ["fuselage", "wing"]:
         shape = recipe[key].value()
-        grunk.reflect.invoke("grocc::BRepTools::Write", shape, f"{key}_face.brep")
-        grunk.reflect.invoke("grocc::export_to_step", shape, f"{key}_face.stp")
+        grunk.reflect.invoke("grocc::BRepTools::Write", shape, f"{key}.brep")
+        grunk.reflect.invoke("grocc::export_to_step", shape, f"{key}.stp")
