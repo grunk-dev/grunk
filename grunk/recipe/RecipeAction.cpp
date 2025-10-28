ef3f763b4085b9c960e61f1182dd9c6c5f97a9a0
Jan Kleinert <jan.kleinert@dlr.de>
2025-10-28 22:04:43 +0100

    move decl to cpp file

--- grunk/recipe/RecipeAction.cpp
+++ grunk/recipe/RecipeAction.cpp
@@ -2,4 +2,12 @@
 
 namespace grunk {
 
+    decltype(auto) RecipeAction::result() const {
+        return this->template res<object>(0);
+    }
+
+    decltype(auto) RecipeAction::argument(int i) const {
+        return this->template arg<object>(i);
+    }
+
 } // namespace grunk
--- grunk/recipe/RecipeAction.hpp
+++ grunk/recipe/RecipeAction.hpp
@@ -12,67 +12,23 @@ class RecipeAction : public parametric::ComputeNode<RecipeAction>
 
 private:
 
-    inline decltype(auto) result() const {
-        return this->template res<object>(0);
-    }
+    decltype(auto) result() const;
 
-    inline decltype(auto) argument(int i) const {
-        return this->template arg<object>(i);
-    }
+    decltype(auto) argument(int i) const;
 
 public:
 
-    using ReturnType = grunk::object;
+    RecipeAction(std::string const& name);
 
-    RecipeAction(std::string const& name) : name(name) {}
+    void connect_inputs(DynamicFeature const& recipe, std::unordered_map<std::string, DynamicFeature> const& input_map);
 
-    void connect_inputs(DynamicFeature const& recipe, std::unordered_map<std::string, DynamicFeature> const& input_map)
-    {
-        depends_on(recipe);
-        for (auto const& [key, value] : input_map) {
-            target_features.push_back(key);
-            depends_on(value);
-        }
-    }
+    DynamicFeature initialize_results() const;
 
-    DynamicFeature initialize_results() const
-    {
-        return feature<object>({});
-    }
+    void connect_results(DynamicFeature const& res);
 
-    void connect_results(DynamicFeature const& res)
-    {
-        computes(res);
-    }
+    void eval() const override;
 
-    void eval() const override
-    {
-
-        auto recipe_obj = argument(0).value();
-        Recipe const& recipe_from = recipe_obj.as<Recipe const&>();
-
-        //TODO: It this expensive?
-        Recipe cloned = recipe_from.clone();
-
-        for (size_t i = 1; i < this->num_parents(); ++i) {
-            auto const& arg = argument(i).value();
-            auto target = cloned.get_feature(target_features[i]);
-            target.set_value(arg);
-        }
-
-        // transform to output
-        if (auto output = result(); output) {
-            sol::state_view lua(recipe_obj.lua_state());
-            auto cloned_obj = sol::make_object(lua, cloned);
-            output->set_value(cloned_obj);
-        }
-    }
-
-    std::string serialize() const override final
-    {
-        //TODO
-        return name;
-    }
+    std::string serialize() const override final;
 
 private:
     std::string name;
