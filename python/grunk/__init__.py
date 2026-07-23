47d6463d7fac28f33835d0fb2f67378d82f54e9e
joergbrech <joergbrech@users.noreply.github.com>
2026-07-23 09:18:30 +0000

    deploy: 500705aaf45e835136f469256d954b245a685a5a

--- python/grunk/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2026 Jan Kleinert <jan.kleinert@dlr.de>
-#
-# SPDX-License-Identifier: MPL-2.0
-
-from ._bindings import __doc__, __version__
-from ._bindings import __default_state__
-from ._bindings import *
--- tests/python/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2026 Jan Kleinert <jan.kleinert@dlr.de>
-#
-# SPDX-License-Identifier: MPL-2.0
